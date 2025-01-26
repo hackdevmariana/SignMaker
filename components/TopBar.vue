a701c6bb9c23666b7b018ac06957d48e1b4e9218
fernando <fernando@juandemariana.org>
2025-01-26 23:07:27 +0100

    Configure styles.css

--- components/TopBar.vue
+++ components/TopBar.vue
@@ -2,12 +2,7 @@
     <div class="bg-gray-800 text-white p-4 flex justify-between items-center">
         <h1 class="text-lg font-bold">SignMaker</h1>
         <div class="flex space-x-4">
-            <ClientOnly>
-                <Phrase />
-            </ClientOnly>
-            <!-- <Text /> -->
-            <Duration />
-            <Play />
+               
         </div>
     </div>
 </template>
@@ -15,9 +10,6 @@
 <script setup>
 import { onMounted } from 'vue';
 import Phrase from '@/components/topbar/Phrase.vue';
-import Duration from '@/components/topbar/Duration.vue';
-import Play from '@/components/topbar/Play.vue';
-
 onMounted(() => {
     console.log("✅ TopBar.vue montado correctamente.");
 });
